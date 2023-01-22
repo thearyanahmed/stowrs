0add7d80dbcf2d9842fe8d0e5a5e800d4f203d81
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2023-01-22 18:25:07 +0100

    Add local file dir check

--- examples/main.rs
+++ examples/main.rs
@@ -1,13 +1,17 @@
-use stowrs::storage::Storage;
+use stowrs::storage::{Storage, StorageConfig};
 
 fn main() {
     println!("running example main");
 
-    let mut storage = Storage::default();
+    let mut default_conf = StorageConfig::default();
 
-    println!("storage exists: {}",storage.dir_exists("/Users/thearyanahmed/rusted/stowrs".to_string()));
+    let conf = default_conf.base_dir("/Users/thearyanahmed/rusted/stowrs".to_string());
 
-    let _= storage.root("/Users/thearyanahmed/rusted".to_string());
+    let mut storage = Storage::new(&conf);
+
+    println!("storage exists: {}",storage.dir_exists("examples".to_string()));
+
+    let _= storage.make_base_dir("/Users/thearyanahmed/rusted".to_string());
 
     storage.get_root()
 }
--- src/storage.rs
+++ src/storage.rs
@@ -1,12 +1,39 @@
+use std::borrow::{Borrow, BorrowMut};
 use std::env;
+use std::fmt::format;
 use crate::errors::DirectoryError;
 use crate::file::File;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 
 pub struct Storage {
     base_directory: String,
     disk: String,
+}
+
+pub trait StorageAdapter {
+
+}
+
+pub struct StorageConfig {
+    pub base_directory: Option<String>,
+    // pub disk_driver: dyn StorageAdapter,
+}
+
+impl Default for StorageConfig {
+    fn default() -> Self {
+        StorageConfig {
+            base_directory: None,
+        }
+    }
+}
+
+impl StorageConfig {
+    pub fn base_dir(&mut self, path: String) -> &mut StorageConfig {
+        self.base_directory =  Some(path);
+
+        &mut *self
+    }
 }
 
 
@@ -17,15 +44,25 @@ impl Default for Storage {
             disk: "disk".to_string(),
         }
     }
-
 }
 
 impl Storage {
-    pub fn new() -> Storage {
-        Storage::default() // @note for now only
+    pub fn new(config: &StorageConfig) -> Storage {
+
+        let dir = match &config.base_directory {
+            None => Storage::get_default_base_directory(),
+            Some(dir) => dir.to_string(),
+        };
+
+        Storage {
+            base_directory: dir,
+            disk: "/".to_string(),
+        }
     }
 
     fn get_default_base_directory() -> String {
+        // @note it should be different for each driver
+
         match env::current_dir() {
             Ok(path_buf) => {
                 match path_buf.to_str() {
@@ -33,16 +70,31 @@ impl Storage {
                     Some(path_str) => path_str.to_string()
                 }
             },
-            Err(_) => { "/".to_string() },
+            Err(_) => "/".to_string(),
         }
     }
 
-    pub fn root(&mut self, path: String) -> &Storage {
+    fn get_full_path_buf(&self, path: String) -> PathBuf {
+        let path_buf = self.to_base_path();
+
+        path_buf.join(&path)
+    }
+
+    fn to_base_path(&self) -> &Path {
+        Path::new(&self.base_directory)
+    }
+
+    pub fn dir_exists(&self, path: String) -> bool {
+        self.get_full_path_buf(path).is_dir()
+    }
+
+    pub fn make_base_dir(&mut self, path: String) -> &Storage {
         self.base_directory = path;
 
         &*self
     }
 
+    // @todo delete
     pub fn get_root(&self) {
         println!("root: {}",self.base_directory)
     }
@@ -60,9 +112,6 @@ impl Storage {
         println!("looking for {}",name)
     }
 
-    pub fn dir_exists(&self, path: String) -> bool {
-        Path::new(&path).is_dir()
-    }
 
     pub fn exists(&self, name: String) -> Result<bool, DirectoryError> {
         Ok(false)
