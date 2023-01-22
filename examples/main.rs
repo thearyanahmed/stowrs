ebd11bc2f2c41d0191d939ddc41c230e52b75206
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2023-01-22 16:38:06 +0100

    Add example

--- examples/main.rs
+++ examples/main.rs
@@ -1,3 +1,14 @@
+use stowrs::storage::Storage;
+
 fn main() {
-    println!("running example main")
+    println!("running example main");
+
+    let storage = Storage::default();
+
+    match storage.dir_exists("hello".to_string()) {
+        Ok(_) => {
+            println!("exists")
+        }
+        Err(err) => println!("{}", err.to_string())
+    }
 }
