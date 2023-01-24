a2b9a2c58a62511b815c9bdd183ebbf26c51234e
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2023-01-24 20:32:59 +0100

    Remove unnecessary example code

--- examples/main.rs
+++ examples/main.rs
@@ -1,35 +1,24 @@
 use std::any::Any;
-use stowrs::storage::{Storage, StorageAdapter};
-use stowrs::adapters::local_file_system_adapter::{LocalFileSystemAdapter, LocalFileSystemAdapterConfig};
+use stowrs::adapters::local_file_system_adapter::{
+    LocalFileSystemAdapter, LocalFileSystemAdapterConfig,
+};
 use stowrs::adapters::s3_file_system_adapter::{S3FileSystemAdapter, S3FileSystemAdapterConfig};
 use stowrs::helpers;
+use stowrs::storage::{Storage, StorageAdapter};
 
 fn main() {
     println!("running example main");
 
-    // let conf = StorageAdapterConfigToBeRemoved{};
-    //
-    // let mut storage = Storage::new(conf);
-    //
-    // println!("storage exists: {}",storage.dir_exists("examples".to_string()));
-    //
-    // let _= storage.make_base_dir("/Users/thearyanahmed/rusted".to_string());
-    //
-    // let _demo_storage_config = DemoStorageAdapterConfig{};
-    // let s3_storage_config = S3StorageAdapterConfig{};
-    //
-    // let hello = Hello::new(s3_storage_config);
-    //
-    // hello.demo_dir_exists();
+    let local_adapter_config = LocalFileSystemAdapterConfig {
+        base_directory: "/Users/thearyanahmed/rusted".to_string(),
+    };
+    let s3_storage_config = S3FileSystemAdapterConfig {
+        bucket: "hello-world".to_string(),
+    };
 
-    // 0.1
+    let storage = Storage::new(helpers::get_adapter("local", &local_adapter_config));
 
-    let local_adapter_config = LocalFileSystemAdapterConfig{ base_directory: "/Users/thearyanahmed/rusted".to_string() };
-    let s3_storage_config = S3FileSystemAdapterConfig{ bucket: "hello-world".to_string() };
-
-    let storage = Storage::new(helpers::get_adapter("local",&local_adapter_config));
-
-    let v : Vec<Box<dyn StorageAdapter>> = vec![
+    let v: Vec<Box<dyn StorageAdapter>> = vec![
         Box::new(LocalFileSystemAdapter::new(&local_adapter_config)),
         Box::new(S3FileSystemAdapter::new(&s3_storage_config)),
     ];
